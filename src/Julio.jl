25844d640a438b979a50429477aad24c080aac06
Takafumi Arakaki <aka.tkf@gmail.com>
2021-08-12 16:23:24 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/Julio.jl
@@ -0,0 +1,11 @@
+baremodule Julio
+
+module Internal
+
+using ..Julio: Julio
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule Julio
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Julio
+using Test
+
+@testset "Julio.jl" begin
+    # Write your tests here.
+end
