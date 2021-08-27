98ccb359270691de105a07e5868d799b8f0dd76e
Takafumi Arakaki <aka.tkf@gmail.com>
2021-08-27 15:19:19 -0400

    Ignore complains about non-exported docstrings

--- docs/make.jl
+++ docs/make.jl
@@ -114,6 +114,7 @@ makedocs(
     modules = [Julio],
     pages = PAGES,
     doctest = false,  # tested via test/runtests.jl
+    checkdocs = :exports,  # ignore complains about non-exported docstrings
     strict = lowercase(get(ENV, "CI", "false")) == "true",
     # See: https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
 )
